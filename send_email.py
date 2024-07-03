a8a024cb8b66b87d256700a51e7ced132c0a07e4
marcus <howardmilleriv23@gmail.com>
2024-07-03 10:16:24 -0400

    Implement sending emails through a form

---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "howardmilleriv23@gmail.com"
+    password = "qsdk ktke wlsp xbbn"
+    receiver = "howardmilleriv23@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+send_email()
