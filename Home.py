7a6ec2d3447218cbe04448a48ff902d4c16df46a
marcus <howardmilleriv23@gmail.com>
2024-07-23 12:21:32 -0400

    Update the Home.py with current projects as well as the data.csv and updated my picture

--- Home.py
+++ Home.py
@@ -7,12 +7,12 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/DSC04678 (1).JPG")
 
 with col2:
     st.title("Howard Miller")
     content = """
-    EMPTY
+    An enthusiastic, high-achieving student with a passion for leadership, academic excellence, and community service, seeking a rewarding college experience. Dedicated to encouraging and empowering others while growing as a leader and student. Desire to pursue a degree in Computer Science and Entrepreneurship.
     """
     st.write(content)
 
@@ -25,15 +25,16 @@ col3, emptycol, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
+
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:2].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/"+row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
+    for index, row in df[2:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/"+row["image"])
